e3485153bf0eb428f57dcaa1504f5e33bb07e8f2
mbrav <mbrav@protonmail.com>
2021-12-16 22:54:27 +0300

    b.py submit

--- sp12/b.py
+++ sp12/b.py
@@ -1,59 +1,76 @@
-# nnnnnnn
+# 61428598
 
 import operator
 from collections import deque
+from typing import List, Union
+Number = Union[int, float]
 
-ops = {
+operations = {
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
-    '/': operator.div,
+    '/': operator.floordiv,
+    # '/': operator.truediv,
 }
 
 
-def in_put():
-    x = int(input())
-    y = int(input())
-    obj = SuperMegaDeck(size=y)
-    for i in range(x):
-        args = input().split()
-        f = operator.methodcaller(args[0])
-        if len(args) == 2:
-            f = operator.methodcaller(args[0], int(args[1]))
-        f(obj)
-    return x, y
+def calc(operation: str, *args: Number) -> Number:
+    op = operations[operation]
+
+    if len(args) == 1:
+        return op(args[0], args[0])
+    if len(args) == 2:
+        return op(args[0], args[1])
+
+    # Екстра функционал для вычислений больше двух аргументов
+    #  ¯\_(ツ)_/¯
+    result = None
+    for a in range(1, len(args)):
+        if a == 1:
+            result = op(args[a-1], args[a])
+        else:
+            result = op(result, args[a])
+    return result
 
 
-class SuperMegaDeck():
-    def __init__(self, size):
+class StackMinimalochka():
+    def __init__(self):
         self.qeue = deque()
 
-    def push_front(self, val):
+    def push(self, val):
         return self.qeue.append(val)
 
-    def push_back(self, val):
-        return self.qeue.appendleft(val)
-
-    def pop_front(self):
+    def pop(self):
         if self.count():
             popped = self.qeue.pop()
-            print(popped)
             return popped
-        return 'error'
-
-    def pop_back(self):
-        if self.count():
-            popped = self.qeue.popleft()
-            print(popped)
-            return popped
-        return 'error'
+        print('error')
 
     def count(self):
         return len(self.qeue)
 
+    def __str__(self):
+        return str(f'{len(self.qeue)} objects in Stack')
+
+
+def in_put(string: str = None) -> List:
+    if string:
+        return string.split()
+    return input().split()
+
 
 if __name__ == '__main__':
-    # in_put()
-    x = 5
-    methodToCall = getattr(math, 'sin')
-    result = methodToCall(x)
+    # inputs = in_put('2 5 - 4 /')
+    inputs = in_put()
+    stack = StackMinimalochka()
+    for i in inputs:
+        if i in operations:
+            y = stack.pop()
+            x = stack.pop()
+            result = calc(i, x, y)
+            stack.push(result)
+        else:
+            stack.push(int(i))
+    result = int(stack.pop())
+
+    print(result)
