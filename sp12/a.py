f031f0bef4e04efdaed4a5ddfcf98f4284aa0402
mbrav <mbrav@protonmail.com>
2021-12-16 21:33:08 +0300

    minimized a.py, add b.py

--- sp12/a.py
+++ sp12/a.py
@@ -1,6 +1,6 @@
-# 61402899
+# 61421264
 
-from operator import methodcaller
+import operator
 from collections import deque
 
 
@@ -10,9 +10,9 @@ def in_put():
     obj = SuperMegaDeck(size=y)
     for i in range(x):
         args = input().split()
-        f = methodcaller(args[0])
+        f = operator.methodcaller(args[0])
         if len(args) == 2:
-            f = methodcaller(args[0], int(args[1]))
+            f = operator.methodcaller(args[0], int(args[1]))
         f(obj)
     return x, y
 
@@ -27,14 +27,12 @@ class SuperMegaDeck():
             self.qeue.append(val)
             return val
         print('error')
-        return 'error'
 
     def push_back(self, val):
         if len(self.qeue) < self.size:
             self.qeue.appendleft(val)
             return val
         print('error')
-        return 'error'
 
     def pop_front(self):
         if self.count():
@@ -42,7 +40,6 @@ class SuperMegaDeck():
             print(popped)
             return popped
         print('error')
-        return 'error'
 
     def pop_back(self):
         if self.count():
@@ -50,95 +47,10 @@ class SuperMegaDeck():
             print(popped)
             return popped
         print('error')
-        return 'error'
 
     def count(self):
-        # print(len(self.qeue))
         return len(self.qeue)
 
 
 if __name__ == '__main__':
     in_put()
-
-    # deck = SuperMegaDeck(size=8)
-    # deck.push_front(842)
-    # deck.pop_back()
-    # deck.push_front(576)
-    # deck.push_front(-853)
-    # deck.pop_back()
-    # deck.push_front(123)
-    # deck.push_front(-236)
-    # deck.pop_front()
-    # deck.push_back(840)
-    # deck.pop_front()
-    # deck.push_back(740)
-    # deck.push_back(347)
-    # deck.pop_front()
-    # deck.push_front(-767)
-    # deck.push_front(-711)
-    # deck.push_back(-7)
-    # deck.pop_front()
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.pop_front()
-    # deck.push_back(-215)
-    # deck.push_front(540)
-    # deck.pop_front()
-    # deck.push_front(-293)
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.pop_front()
-    # deck.pop_back()
-    # deck.push_back(873)
-    # deck.push_front(47)
-    # deck.push_back(-238)
-    # deck.push_front(-575)
-    # deck.pop_front()
-    # deck.push_front(-916)
-    # deck.push_front(292)
-    # deck.push_back(302)
-    # deck.push_front(456)
-    # deck.push_back(92)
-    # deck.push_back(-422)
-    # deck.push_back(890)
-    # deck.push_back(-100)
-    # deck.pop_back()
-    # deck.push_front(-356)
-    # deck.pop_front()
-    # deck.push_front(430)
-    # deck.push_front(469)
-    # deck.push_back(-56)
-    # deck.push_front(273)
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.push_front(-397)
-    # deck.push_back(131)
-    # deck.pop_front()
-    # deck.push_back(-4)
-    # deck.push_front(-265)
-    # deck.push_back(10)
-    # deck.push_back(-531)
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.push_front(766)
-    # deck.pop_front()
-    # deck.push_front(-520)
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.pop_back()
-    # deck.push_front(-386)
-    # deck.push_back(-320)
-    # deck.push_back(21)
-    # deck.pop_front()
-    # deck.push_back(495)
-    # deck.pop_front()
-    # deck.push_front(-95)
-    # deck.pop_front()
-    # deck.pop_back()
-    # deck.push_back(908)
-    # deck.push_front(115)
--- sp12/b.py
+++ sp12/b.py
@@ -1,1 +1,59 @@
 # nnnnnnn
+
+import operator
+from collections import deque
+
+ops = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.div,
+}
+
+
+def in_put():
+    x = int(input())
+    y = int(input())
+    obj = SuperMegaDeck(size=y)
+    for i in range(x):
+        args = input().split()
+        f = operator.methodcaller(args[0])
+        if len(args) == 2:
+            f = operator.methodcaller(args[0], int(args[1]))
+        f(obj)
+    return x, y
+
+
+class SuperMegaDeck():
+    def __init__(self, size):
+        self.qeue = deque()
+
+    def push_front(self, val):
+        return self.qeue.append(val)
+
+    def push_back(self, val):
+        return self.qeue.appendleft(val)
+
+    def pop_front(self):
+        if self.count():
+            popped = self.qeue.pop()
+            print(popped)
+            return popped
+        return 'error'
+
+    def pop_back(self):
+        if self.count():
+            popped = self.qeue.popleft()
+            print(popped)
+            return popped
+        return 'error'
+
+    def count(self):
+        return len(self.qeue)
+
+
+if __name__ == '__main__':
+    # in_put()
+    x = 5
+    methodToCall = getattr(math, 'sin')
+    result = methodToCall(x)
