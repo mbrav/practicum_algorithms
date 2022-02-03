f480b678621cb203eea7aa32e414c637efc6c0f3
mbrav <mbrav@protonmail.com>
2022-02-03 16:14:48 +0300

    K complete

--- sp13/theory/k.py
+++ sp13/theory/k.py
@@ -1,59 +1,36 @@
-# nnnnnnn
-
-from typing import List
+# 64786828
 
 
-def in_put(string: str = None):
-    def str_or_arr(st: str):
-        st = st.split(' ')
-        # if len(st) > 1:
-        #     return st
-        # return st[0]
-        return st
-    if string:
-        return str_or_arr(string)
-    return str_or_arr(input())
-
-
-def merge(arr: list, left: int, mid: int, right: int) -> list:
+def merge(arr: list, i_left: int, mid: int, i_right: int) -> list:
     # Your code
     # “ヽ(´▽｀)ノ”
-    right = len(arr)
-    mid = right // 2
-    arr1, arr2 = arr[left:mid], arr[mid:right]
-    i = j = 0
-    result = []
-    while i < mid - left and j < right - mid:
-        if arr1[i] <= arr2[j]:
-            result.append(arr1[i])
-            i += 1
+    left = arr[i_left: mid]
+    right = arr[mid: i_right]
+    l, r, k = 0, 0, i_left
+    while i_left + l < mid and mid + r < i_right:
+        if left[l] <= right[r]:
+            arr[k] = left[l]
+            l += 1
         else:
-            result.append(arr2[j])
-            j += 1
-    result += arr1[i:] + arr2[j:]
-    return result
+            arr[k] = right[r]
+            r += 1
+        k += 1
+    while i_left + l < mid:
+        arr[k] = left[l]
+        l += 1
+        k += 1
+    while mid + r < i_right:
+        arr[k] = right[r]
+        r += 1
+        k += 1
+    return arr
 
 
 def merge_sort(arr: list, left: int, right: int) -> None:
     # Your code
     # “ヽ(´▽｀)ノ”
-    if left != 0 or right != len(arr):
-        arr = arr[left: right]
-    arr = sorted(arr)
-    return arr
-
-
-def main():
-    a = in_put()
-    b = in_put()
-    c = in_put()
-    d = in_put()
-
-    numbers = list(map(lambda x: int(x), c))
-    result = merge_sort(numbers, 0, len(numbers))
-    result = list(map(lambda x: str(x), result))
-    print(' '.join(result))
-
-
-if __name__ == '__main__':
-    main()
+    if right - left > 1:
+        mid = (left + right) // 2
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid, right)
+        arr[left: right] = merge(arr, left, mid, right)[left: right]
